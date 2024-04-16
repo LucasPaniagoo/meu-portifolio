7dcb135096d6467ad6df62ce463de398c91053ae
Lucas Paniago <lucaspaniagosantos@outlook.com>
2024-04-16 15:43:17 -0400

    Muda o modo light
    Alteração no propriedade "secundary" do modo light.

--- src/themes/light.jsx
+++ src/themes/light.jsx
@@ -1,8 +1,8 @@
 export const lightTheme = {
     primary: '#B8B7B3',
-    secundary: '#747371',
+    secundary: '#858381',
     background: '#E4E2DD',
-    text: '#E4E2DD',
+    text: '#FFFFFF',
     nav: '#747371',
     title: '#747371',
     badge: '#E4E2DD'
